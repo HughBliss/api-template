79c688f422037e3ceab344cebb0beba134263817
Alexey Korzhavin <hughwols@gmail.com>
2019-10-30 11:08:36 +0200

    add order model

--- DataBase/Context.cs
+++ DataBase/Context.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Study.Models;
 
 namespace Study.DataBase
 {
@@ -8,5 +9,6 @@ namespace Study.DataBase
             : base(options)
         {
         }
+        public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ Models/OrderModel.cs
@@ -0,0 +1,10 @@
+namespace Study.Models
+{
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string ProductName { get; set; }
+    }
+}
