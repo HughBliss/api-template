5b89b5448258b013bf40c1dd6e109153ed1b6db9
Alexey Korzhavin <hughwols@gmail.com>
2019-10-30 11:19:22 +0200

    add AllOrders page to client app

---
+++ client-app/src/views/AllOrders.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="AllOrders">
+    <table>
+      <tbody>
+        <tr>
+          <td>ID</td>
+          <td>Client Name</td>
+          <td>Product Name</td>
+        </tr>
+        <tr v-for="order in orders" :key="order.id">
+          <td>{{order.id}}</td>
+          <td>{{order.clientName}}</td>
+          <td>{{order.productName}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    orders: []
+  }),
+  created() {
+    this.$axios.get("https://localhost:5001/api/orders").then(res => {
+      this.orders = res.data;
+      console.log(res);
+    });
+  }
+};
+</script>
